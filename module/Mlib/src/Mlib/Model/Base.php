248e1377244313ff8e4b1af08851b1955fe0a570
mattjs <spenc.matthew@gmail.com>
2013-07-01 23:31:47 -0400

    test user class

---
+++ module/Mlib/src/Mlib/Model/Base.php
@@ -0,0 +1,12 @@
+<?php
+namespace Mlib;
+
+use Zend\Db\Adapter\Adapter;
+use Zend\Db\TableGateway\AbstractTableGateway;
+
+class Base extends AbstractTableGateway {
+	
+    public function __construct(Adapter $adapter) {
+        $this->adapter = $adapter;
+    }
+}
---
+++ module/Mlib/src/Mlib/Model/User.php
@@ -0,0 +1,10 @@
+<?php
+namespace Mlib;
+
+use Mlib\Base;
+
+class User extends Base {
+	public function test() {
+		return 'hello';
+	}
+}
