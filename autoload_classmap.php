be839cd9de16816a95f0716fbb8951883cb121ee
mattjs <spenc.matthew@gmail.com>
2013-07-03 00:30:30 -0400

    stash

---
+++ autoload_classmap.php
@@ -0,0 +1,6 @@
+<?php
+return array(
+    'Mlib\Module'                                            => __DIR__ . '/Module.php',
+    'Mlib\Model\Base'                              => __DIR__ . '/src/Mlib/Model/Base.php',
+    'Mlib\Model\User'                              => __DIR__ . '/src/Mlib/Model/User.php',
+);
