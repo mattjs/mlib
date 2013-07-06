ed0cc3a0325987c0abaf6a5969731df3b770067d
mattjs <spenc.matthew@gmail.com>
2013-07-06 11:54:02 -0400

    testing

---
+++ src/Mlib/Model/Session.php
@@ -0,0 +1,21 @@
+<?php
+namespace Mlib\Model;
+
+use Mlib\Model\Base;
+
+class Session extends Base {
+	public $token_name;
+	
+	
+	public function start(Array $session) {
+		
+	}
+	
+	public function destroy() {
+		
+	}
+	
+	public function valid() {
+		
+	}
+}
--- src/Mlib/Model/User.php
+++ src/Mlib/Model/User.php
@@ -2,9 +2,32 @@
 namespace Mlib\Model;
 
 use Mlib\Model\Base;
+use Mlib\Model\Session;
 
 class User extends Base {
-	public function test() {
-		return 'hello';
+	private $session;
+	private $properties = array('session');
+	
+	public function login() {
+		
+	}
+	
+	public function logout() {
+		
+	}
+	
+	public function __get($name) {
+		if(isset($this->${$name}) && $this->${$name}) {
+			return $this->${$name};
+		} else if(in_array($this->properties, $name)){
+			return $this->${'_init_'.$name}();
+		} else {
+			return $this->__get_error();
+		}
+	}
+	
+	protected function _init_session() {
+		$this->session = new Session($this->adapter);
+		return $this->session;
 	}
 }
