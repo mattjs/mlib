65f389327c2472d40c63fca74f1b65f917d1fd66
mattjs <spenc.matthew@gmail.com>
2013-07-21 17:13:16 -0400

    testing

--- src/Mlib/Model/Base.php
+++ src/Mlib/Model/Base.php
@@ -9,4 +9,15 @@ class Base extends AbstractTableGateway {
     public function __construct(Adapter $adapter) {
         $this->adapter = $adapter;
     }
+	
+	protected function is_unique($field_name, $value) {
+		return !!$this->select(array($field_name => $value))->current();
+	}
+	
+	protected function duplicate_entry_error($field_name) {
+		$error = array();
+		$error['name'] = 'DuplicateEntry';
+		$error['field'] = $field_name;
+		return $error;
+	}
 }
