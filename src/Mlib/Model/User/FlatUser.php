ec88c87346f07133695b17df3a89dd3a36f0324b
mattjs <spenc.matthew@gmail.com>
2013-07-22 18:13:16 -0400

    stash

---
+++ src/Mlib/Model/User/FlatUser.php
@@ -0,0 +1,21 @@
+<?php
+namespace Mlib\Model\User;
+
+class FlatUser {
+	public $logged_in;
+	protected $details;
+	protected $session;
+	
+	public function __construct($logged_in, $details, $session) {
+		$this->logged_in = $logged_in;
+		$this->details = $details;
+		$this->session = $session;
+	}
+	
+	protected function __get($name) {
+		if(array_key_exists($name, $details)) {
+			return $this->details[$name];
+		}
+		return null;
+	}
+}
