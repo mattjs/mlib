3492e733ea15277ce27db8cdc6ca8924f55a738e
mattjs <spenc.matthew@gmail.com>
2013-07-27 22:58:33 -0400

    add select to form

--- src/Mlib/Form/Field.php
+++ src/Mlib/Form/Field.php
@@ -8,13 +8,17 @@ class Field {
 	public $title;
 	public $required;
 	public $verifies;
+	public $options;
 	
 	protected $properties = array();
 	
 	public function __construct(Array $info) {
 		$this->name = $info['name'];
-		$this->type = $info['type'];
 		$this->element = $info['element'];
+		
+		if(isset($info['type'])) {
+			$this->type = $info['type'];
+		}
 		
 		if(isset($info['title'])) {
 			$this->title = $info['title'];
@@ -23,6 +27,10 @@ class Field {
 		if(isset($info['properties'])) {
 			$this->properties = $info['properties'];
 		}
+		
+		if($this->element == 'select') {
+			$this->options = $info['options'];
+		} 
 	}
 	
 	public function set_property($name, $value) {
