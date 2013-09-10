fb2c31df26f1baf8a97f2e25380a9e1369e01f87
mattjs <spenc.matthew@gmail.com>
2013-09-10 01:34:02 -0400

    put request

--- src/Mlib/Http/Http.php
+++ src/Mlib/Http/Http.php
@@ -45,16 +45,12 @@ class Http {
 		return $result;
 	}
 	
-	public static function put($url, $data=array()) {
+	public static function put($url) {
 		$curl = self::init();
 		curl_setopt_array($curl, array(
 			CURLOPT_URL => $url,
-			CURLOPT_CUSTOMREQUEST => "PUT"
+			CURLOPT_PUT => 1
 		));
-		
-		if(!empty($data)) {
-			curl_setopt($curl, CURLOPT_POSTFIELDS, http_build_query($data));
-		}
 		
 		$result = curl_exec($curl);
 		if($result === false) {
